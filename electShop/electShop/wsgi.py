56d15682940fcdeb125ba8d4f395231cb85a1906
m1kshoj <mhlsntsn@icloud.com>
2022-04-21 01:46:25 +0300

    new shop 4 test

---
+++ electShop/electShop/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for electShop project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'electShop.settings')
+
+application = get_wsgi_application()
